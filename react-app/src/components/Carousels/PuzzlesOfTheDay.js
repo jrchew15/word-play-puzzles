eb11d2e0175123be1e4f4d2ac852626b0a7ef888
Jason Chew <jrchew15@gmail.com>
2022-10-03 16:35:28 -0700

    carousel cleanup

--- react-app/src/components/Carousels/PuzzlesOfTheDay.js
+++ react-app/src/components/Carousels/PuzzlesOfTheDay.js
@@ -20,16 +20,20 @@ export default function PuzzlesOfTheDay() {
     return (
         <>
             <h2>Today's Puzzle:</h2>
-            {puzzles.length > 0 && <div id='the-pod' onClick={() => history.push(`/wordgons/${puzzles[0].id}`)} style={{ cursor: 'pointer', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            {puzzles.length > 0 && <div id='the-pod' style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', width: 'min-content' }}>
                 <h3>{puzzles[0].puzzleDay}</h3>
-                <ListableBoxAndLetters letters={puzzles[0].letters} puzzleId={puzzles[0].id} />
+                <div onClick={() => history.push(`/wordgons/${puzzles[0].id}`)} style={{ cursor: 'pointer', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <ListableBoxAndLetters letters={puzzles[0].letters} puzzleId={puzzles[0].id} />
+                </div>
             </div>}
             <h2>Past Puzzles of the Day</h2>
             <div id='pod-carousel' className="carousel">
                 {puzzles.slice(1).map(puzzle => (
-                    <div onClick={() => history.push(`/wordgons/${puzzle.id}`)} style={{ cursor: 'pointer', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                         <h3>{puzzle.puzzleDay}</h3>
-                        <ListableBoxAndLetters letters={puzzle.letters} puzzleId={puzzle.id} />
+                        <div onClick={() => history.push(`/wordgons/${puzzle.id}`)} style={{ cursor: 'pointer', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <ListableBoxAndLetters letters={puzzle.letters} puzzleId={puzzle.id} />
+                        </div>
                     </div>
                 ))}
             </div>
