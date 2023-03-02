2a9469d70ad6e07379855b1ed61e7c6264e37c22
Jason Chew <jrchew15@gmail.com>
2023-03-02 06:41:49 -0800

    frontend bug

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -5,12 +5,14 @@ import { useHistory, useParams } from 'react-router-dom';
 import { defaultImg } from '../store/utils/image_urls';
 import { wordGonStats, wordleStats } from '../utils/userStatFetches';
 import './user.css'
+import './Wordle/wordle-puzzle.css'
 
 function User({ userId, setShowModal }) {
   userId = useParams().userId || userId
   const [user, setUser] = useState(null);
   const currentUser = useSelector(state => state.session.user);
-  const [stats, setStats] = useState(null);
+  const [stats, setStats] = useState({});
+  const [styles, setStyles] = useState({});
   const history = useHistory();
 
   useEffect(() => {
@@ -21,18 +23,31 @@ function User({ userId, setShowModal }) {
         let found = await res.json();
         setUser(found);
       }
-      const allStats = {}
       let res2 = await wordGonStats(userId);
       if (res2) {
-        allStats.wordGonStats = res2
+        stats.wordGonStats = res2
       }
       let res3 = await wordleStats(userId);
       if (res3) {
-        allStats.wordleStats = res3
+        stats.wordleStats = res3
       }
-      setStats(allStats)
+      setStats(stats)
     })()
   }, [userId])
+
+  useEffect(() => {
+    if (!stats || !stats.wordleStats) return
+    let max = Math.max(...Object.values(stats.wordleStats));
+
+    for (let i = 1; i <= 6; i++) {
+      if (stats.wordleStats[i]) {
+        let width = Math.round(100 * stats.wordleStats[i] / max) + '%';
+        styles[i] = { width }
+      } else {
+        styles[i] = { display: 'none' }
+      }
+    }
+  }, [stats])
 
   function directToSettings() {
     if (setShowModal) {
@@ -53,11 +68,20 @@ function User({ userId, setShowModal }) {
         Edit Settings
       </button>}
       {stats && <div>
-        <div>WordGon Stats:
+        {/* <div>WordGon Stats:
           {Object.keys(stats.wordGonStats).map((key) => <span>{`${key}:${stats.wordGonStats[key]}`}</span>)}
-        </div>
+        </div> */}
         <div>Wordle Stats:
-          {Object.keys(stats.wordleStats).map((key) => <span>{`${key}:${stats.wordleStats[key]}`}</span>)}
+          < ul id={'modal-stats'} >
+            {
+              [6, 5, 4, 3, 2, 1].map(num => (<li key={num} style={{ display: 'flex', justifyContent: 'left', alignItems: 'center' }}>
+                <span>{num}:</span>
+                <div className={'statbar'} style={styles[num]} />
+                <span>{stats[num]}</span>
+              </li>)
+              )
+            }
+          </ul >
         </div>
       </div>}
     </div>
