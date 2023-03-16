623d7460db1db6bdff75295608058a0c7b5fa3a1
Jason Chew <jrchew15@gmail.com>
2023-03-16 13:44:16 -0700

    total puzzles solved on user modal

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -10,7 +10,6 @@ function User({ userId, setShowModal }) {
   userId = useParams().userId || userId
   const [user, setUser] = useState(null);
   const currentUser = useSelector(state => state.session.user);
-  const [stats, setStats] = useState(null);
   const history = useHistory();
 
   useEffect(() => {
@@ -21,16 +20,6 @@ function User({ userId, setShowModal }) {
         let found = await res.json();
         setUser(found);
       }
-      const allStats = {}
-      let res2 = await wordGonStats(userId);
-      if (res2) {
-        allStats.wordGonStats = res2
-      }
-      let res3 = await wordleStats(userId);
-      if (res3) {
-        allStats.wordleStats = res3
-      }
-      setStats(allStats)
     })()
   }, [userId])
 
@@ -48,18 +37,18 @@ function User({ userId, setShowModal }) {
   return (
     <div id='user-details'>
       <img src={user.profilePicture} alt={user.username} onError={(e) => e.target.src = defaultImg} />
-      <span>{user.username}</span>
+      <span style={{ fontSize: '1.5em' }}>{user.username}</span>
+      <div id='user-puzzle-count'>
+        <span style={{ display: 'flex' }}><p style={{ width: 'min-content' }}>Total Wordles Solved</p>
+          <span className='number-solved'>{user.totalWordles}</span>
+        </span>
+        <span style={{ display: 'flex' }}><p style={{ width: 'min-content' }}>Total Wordgons Solved</p>
+          <p className='number-solved'>{user.totalWordgons}</p>
+        </span>
+      </div>
       {currentUser.id === user.id && <button className='modal-button' onClick={directToSettings}>
         Edit Settings
       </button>}
-      {stats && <div>
-        <div>WordGon Stats:
-          {Object.keys(stats.wordGonStats).map((key) => <span>{`${key}:${stats.wordGonStats[key]}`}</span>)}
-        </div>
-        <div>Wordle Stats:
-          {Object.keys(stats.wordleStats).map((key) => <span>{`${key}:${stats.wordleStats[key]}`}</span>)}
-        </div>
-      </div>}
     </div>
   )
 }
