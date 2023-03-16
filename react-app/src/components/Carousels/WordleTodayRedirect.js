cac853e6b96e669f29a4270bd251a455bfeaedc3
Jason Chew <jrchew15@gmail.com>
2023-03-16 15:06:01 -0700

    probably working, wsl clock out of sync

--- react-app/src/components/Carousels/WordleTodayRedirect.js
+++ react-app/src/components/Carousels/WordleTodayRedirect.js
@@ -1,18 +1,27 @@
 import { useState, useEffect } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
+import { makeRandomWordle } from "../Wordle/wordleFunctions";
 
 export default function WordleTodayRedirect() {
     const now = new Date();
+    const history = useHistory();
     let dateString = [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('-');
-
+    console.log('dateString', dateString)
     const [wordle, setWordle] = useState(null)
 
     useEffect(() => {
         (async () => {
             const res = await fetch(`/api/wordles/by_date/${dateString}`)
-
-            const data = await res.json()
-            setWordle(data)
+            if (res.ok) {
+                const data = await res.json()
+                setWordle(data)
+                return
+            }
+            const resBody = await res.json()
+            if (resBody.errors.includes('Puzzle not found')) {
+                const data = await makeRandomWordle(history, false, false, true);
+                setWordle(data);
+            }
         })()
     }, [])
 
